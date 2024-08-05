a79332bb37804620aa10fb8a491904a22ad03c98
Stello124 <kartal.enbiya@gmail.com>
2024-08-05 21:32:06 +0300

    enums created

---
+++ Assets/Scripts/Game/Enums/Direction.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum Direction
+{
+    None = -1,
+    Up = 0,
+    UpRight,
+    Right,
+    DownRight,
+    Down,
+    DownLeft,
+    Left,
+    UpLeft
+
+}
---
+++ Assets/Scripts/Game/Enums/MatchType.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum MatchType 
+{
+    None = 0,
+    Green,
+    Yellow,
+    Blue,
+    Red,
+    Pink,
+    Purple,
+    SpecialType
+}
