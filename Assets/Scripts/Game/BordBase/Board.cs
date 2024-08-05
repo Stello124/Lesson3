d5761ff5289f70be1e8d937c6aa869038618922a
Stello124 <kartal.enbiya@gmail.com>
2024-08-05 21:38:28 +0300

    added get neighbour from direction to board

--- Assets/Scripts/Game/BordBase/Board.cs
+++ Assets/Scripts/Game/BordBase/Board.cs
@@ -38,6 +38,54 @@ public class Board : MonoBehaviour
         var cell = signal.Touchable.gameObject.GetComponent<Cell>();
     }
 
+    private Cell GetNeighbourWithDirection(Cell cell, Direction direction)
+    {
+        var x = cell.X;
+        var y = cell.Y;
+
+        switch (direction)
+        {
+            case Direction.None:
+                break;
+            case Direction.Up:
+                y += 1;
+                break;
+            case Direction.UpRight:
+                y += 1;
+                x += 1;
+                break;
+            case Direction.Right:
+                x += 1;
+                break;
+            case Direction.DownRight:
+                y -= 1;
+                x += 1;
+                break;
+            case Direction.Down:
+                y -= 1;
+                break;
+            case Direction.DownLeft:
+                y -= 1;
+                x -= 1;
+                break;
+            case Direction.Left:
+                x -= 1;
+                break;
+            case Direction.UpLeft:
+                y += 1;
+                x -= 1;
+                break;
+            default:
+                throw new ArgumentOutOfRangeException(nameof(direction), direction, null);
+
+        }
+        if (x < 0 || x >= Rows || y < 0 || y >= Cols)
+        {
+            return null;
+        }
+
+        return Cells[x, y];
+    }
     private void CreateCells()
     {
         for (int x = 0; x < Rows; x++)
